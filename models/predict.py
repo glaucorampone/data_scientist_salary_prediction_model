9eaa55b9980ee61a013d0b93bcc9b73c6ded9f15
glaucorampone <117575775+glaucorampone@users.noreply.github.com>
2023-08-06 10:02:07 +0200

    Add files via upload

---
+++ models/predict.py
@@ -0,0 +1,81 @@
+import os
+import pandas as pd
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Define the specific file names for test and cross-validation sets
+train_file_name = "train_set.csv"
+test_file_name = "test_set.csv"
+cv_file_name = "cv_set.csv"
+glauco_file_name = "glauco.csv"
+
+# Join the common path with the specific file names
+test_file_path = os.path.join(common_path, test_file_name)
+cv_file_path = os.path.join(common_path, cv_file_name)
+train_file_path = os.path.join(common_path, train_file_name)
+glauco_file_path = os.path.join(common_path, glauco_file_name)
+
+# Load the test set and cross-validation set into pandas DataFrames
+test_data = pd.read_csv(test_file_path)
+cv_data = pd.read_csv(cv_file_path)
+train_data = pd.read_csv(train_file_path)
+glauco = pd.read_csv(glauco_file_path)
+
+# Separate features (X) and target (y)
+X = train_data.drop(columns=["salary_in_usd"])  # Drop the "salary_in_usd" column to get features
+y = train_data["salary_in_usd"]  # Get the "salary_in_usd" column as the target
+x_cv = cv_data.drop(columns=["salary_in_usd"])
+y_cv = cv_data["salary_in_usd"]
+x_test = test_data.drop(columns=["salary_in_usd"])
+y_test = test_data["salary_in_usd"]
+
+# Display the shapes of X and y
+print("Shape of X:", X.shape)
+print("Shape of y:", y.shape)
+print("Shape of glauco:",glauco.shape)
+# X has 259 features!
+
+# Create the model
+model = Sequential ([
+    Dense(units = 64, activation = 'relu'),
+    Dense(units = 10, activation = 'relu'), #10 is the number of original features
+    Dense(units = 1)
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model on the training data and store the history
+history = model.fit(X, y, batch_size=32, epochs=100, validation_data=(x_cv, y_cv))
+
+# Plot the epoch vs. the cost function
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss (Mean Squared Error)')
+plt.legend()
+plt.title('Training and Validation Loss Over Epochs')
+plt.show()
+
+# Visualize the dataset
+# Assuming you have a specific feature column index you want to plot (e.g., 0 for the first feature)
+feature_column_index = 0
+
+plt.scatter(X[:, feature_column_index], y, label='Training Data')
+plt.xlabel('Feature')
+plt.ylabel('Salary (USD)')
+plt.legend()
+plt.title('Dataset Visualization')
+plt.show()
+
+
+
+
+
+
+
---
+++ script/Data treatment.R
@@ -0,0 +1,85 @@
+setwd("C:\\Users\\HP\\Desktop\\Progetti Data Science\\Previsione salari")
+
+data <- read.csv("ds_salaries.csv")
+head(data)
+
+# Eliminate useless features
+data$salary_currency <- data$salary <- NULL
+
+# Normalize remote_ratio
+data$remote_ratio <- data$remote_ratio/100
+
+#Your Glauco data frame
+glauco <- data.frame(
+  work_year = 2023,
+  experience_level = "EN",
+  employment_type = "FT",
+  job_title = "Data Scientist",
+  employee_residence = "IT",
+  remote_ratio = 0,
+  company_location = "CH",
+  company_size = "L"
+)
+
+# Store the names of columns to exclude from one-hot encoding
+exclude_columns <- c("remote_ratio", "salary_in_usd")
+
+# Extract the names of columns to be one-hot encoded
+cols_to_encode <- setdiff(names(data), exclude_columns)
+
+# Perform one-hot encoding for each column
+for (col in cols_to_encode) {
+  unique_values <- unique(data[[col]])
+  for (val in unique_values) {
+    data[paste(col, "_", val, sep = "")] <- ifelse(data[[col]] == val, 1, 0)
+    glauco[paste(col, "_", val, sep = "")] <- ifelse(glauco[[col]] == val, 1, 0)
+  }
+}
+
+# Remove the original columns as they are no longer needed
+data <- data[, !names(data) %in% cols_to_encode]
+glauco <- glauco[, !names(glauco) %in% cols_to_encode]
+
+# View the resulting encoded dataframe
+head(data)
+head(glauco)
+
+# Split the dataset in Training, CV and Test set
+# Set a seed for reproducibility (optional)
+set.seed(123)
+
+# Step 1: Shuffle the dataset randomly
+shuffled_data <- data[sample(nrow(data)), ]
+
+# Step 2: Define the proportions for each set (adjust these as needed)
+train_ratio <- 0.7   # 70% of data for training
+cv_ratio <- 0.15     # 15% of data for cross-validation
+test_ratio <- 0.15   # 15% of data for test
+
+# Calculate the number of samples for each set based on proportions
+num_samples <- nrow(shuffled_data)
+num_train <- round(train_ratio * num_samples)
+num_cv <- round(cv_ratio * num_samples)
+
+# Create the training, cross-validation, and test sets
+train_set <- shuffled_data[1:num_train, ]
+cv_set <- shuffled_data[(num_train + 1):(num_train + num_cv), ]
+test_set <- shuffled_data[(num_train + num_cv + 1):num_samples, ]
+
+# View the dimensions of each set
+cat("Training set size:", nrow(train_set), "\n")
+cat("Cross-validation set size:", nrow(cv_set), "\n")
+cat("Test set size:", nrow(test_set), "\n")
+
+# Save
+output_dir <- "Modello Buono"  
+write.csv(train_set, file.path(output_dir, "train_set.csv"), row.names = FALSE)
+write.csv(cv_set, file.path(output_dir, "cv_set.csv"), row.names = FALSE)
+write.csv(test_set, file.path(output_dir, "test_set.csv"), row.names = FALSE)
+write.csv(glauco,file.path(output_dir,"glauco.csv"), row.names = FALSE)
+
+
+
+
+
+
